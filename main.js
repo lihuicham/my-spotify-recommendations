ae8a324864c5d9d1658e880a74adf70558e1a030
Li Hui <lihuicham@gmail.com>
2022-06-13 14:42:58 +0800

    Complete all sections and JavaScript

--- main.js
+++ main.js
@@ -1,3 +1,54 @@
+/*============ NAVIGATION ============*/
+const tabs = document.querySelectorAll('[data-target]');
+const tabContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => { 
+        const target = document.querySelector(tab.dataset.target)
+        console.log('hi')
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active-section')
+        })
+
+        target.classList.add('active-section')
+
+        tabs.forEach(tab => {
+            tab.classList.remove('active-section')
+        })
+        tab.classList.add('active-section')
+    })
+})
+
+
+
+/*============ ARTISTS MODAL ============*/
+
+const modalViews = document.querySelectorAll('.artist_modal');
+const modalCards = document.querySelectorAll('.artist_card');
+const modalCloses = document.querySelectorAll('.artist_modal-cross');
+
+let modal = function(modalClick) {
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalCards.forEach((modalCard, i) => {
+    modalCard.addEventListener('click', () => {
+        modal(i);    
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
+
+
+/*============ MIXITUP FILTER FOR ALBUMS ============*/
+
 let mixer = mixitup('.albums_container', {
     selectors: {
         target: '.album_card'
@@ -7,12 +58,15 @@ let mixer = mixitup('.albums_container', {
     }
 });
 
-/* Active Link Work */
-const linkWork = document.querySelectorAll('.album_filter')
+/* Active Link Album */
+const linkAlbum = document.querySelectorAll('.album_filter')
 
 function activeAlbum() {
-    linkWork.forEach(item => item.classList.remove('active-album'))
+    linkAlbum.forEach(item => item.classList.remove('active-album'))
     this.classList.add('active-album')
 }
 
-linkWork.forEach(item => item.addEventListener('click', activeAlbum))
+linkAlbum.forEach(item => item.addEventListener('click', activeAlbum))
+
+
+
